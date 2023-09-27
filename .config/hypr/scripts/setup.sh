f4424744a93196e9509a0418de928f5058280a64
SwayKh <khatrisumit712@gmail.com>
2023-09-27 23:01:16 +0530

    Change setup.sh
    Changed the setup.sh layout for opening dev setup
    also added path to odinproject directory to lfrc

--- .config/hypr/scripts/setup.sh
+++ .config/hypr/scripts/setup.sh
@@ -4,7 +4,9 @@ hyprctl dispatch exec [workspace 1 silent] firefox
 sleep 1
 hyprctl dispatch exec [workspace 2 silent] code
 sleep 1
-hyprctl dispatch exec [workspace 3 silent] "kitty -e lf"
+hyprctl dispatch exec [workspace 3 silent] firefox
+sleep 1
+hyprctl dispatch exec [workspace 4 silent] "kitty -e lf"
 sleep 1
 hyprctl dispatch exec [workspace 4 silent] kitty
 sleep 1
