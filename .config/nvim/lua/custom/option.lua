cd03e01c48da7e9a49dde7a58e60cf73c194ee2c
SwayKh <khatrisumit712@gmail.com>
2023-11-05 23:13:39 +0530

    Update Nvim Editor options, also removed useless comments

--- .config/nvim/lua/custom/option.lua
+++ .config/nvim/lua/custom/option.lua
@@ -1,61 +1,47 @@
 -- [[ Setting options ]]
 -- See `:help vim.o`
--- NOTE: You can change these options as you wish!
 
--- vim.o.disable_netrw = true
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- Set highlight on search
-vim.o.hlsearch = false
-vim.o.incsearch = true
-
-vim.o.cursorline = true
-vim.o.autowrite = true
-vim.o.autoread = true
-vim.o.colorcolumn = 80
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.cursorline = true
+vim.opt.autowrite = true
+vim.opt.autoread = true
+vim.opt.colorcolumn = 80
+vim.opt.laststatus = 3
 
 -- Fix Tab length
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.expandtab = true
-vim.o.shiftround = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftround = true
 
--- Make line numbers default and add relative line numbers
+vim.opt.termguicolors = true
 vim.wo.number = true
 vim.wo.relativenumber = true
+vim.opt.numberwidth = 2
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.smartindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-vim.o.smartindent = true
-
--- Save undo history
-vim.o.undofile = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- Keep scape above and below when scrolling
-vim.o.scrolloff = 10
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.scrolloff = 10
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.opt.completeopt = 'menuone,noselect'
 
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
