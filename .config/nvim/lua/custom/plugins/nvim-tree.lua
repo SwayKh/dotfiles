3ccdaab2378b47f853c55d039303ef6c9ae147e6
SwayKh <khatrisumit712@gmail.com>
2023-11-07 00:17:49 +0530

    Replace CHADtree with nvim-tree

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,19 +1,34 @@
-if true then
-        return {}
-end
 return {
-  {"kyazdani42/nvim-tree.lua",
+  {
+    'kyazdani42/nvim-tree.lua',
     config = function()
-      require("nvim-tree").setup()
-      require("nvim-tree").setup({
-        sort_by = "case_sensitive",
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      local function my_on_attach(bufnr)
+        local api = require 'nvim-tree.api'
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- custom mappings
+        vim.keymap.set('n', 'b', api.tree.change_root_to_node, opts 'Up')
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+      end
+      require('nvim-tree').setup()
+      require('nvim-tree').setup {
+        on_attach = my_on_attach,
+        sort_by = 'case_sensitive',
         open_on_tab = true,
         disable_netrw = true,
         hijack_netrw = true,
         update_cwd = true,
         hijack_cursor = true,
         view = {
-          width = 30,
+          width = 40,
         },
         filters = {
           dotfiles = true,
@@ -25,31 +40,16 @@ return {
           open_file = {
             resize_window = true,
           },
+          expand_all = {
+            max_folder_discovery = 300,
+            exclude = { '.git', 'target', 'build' },
+          },
         },
         renderer = {
           group_empty = true,
           highlight_git = true,
           root_folder_modifier = ':t',
           icons = {
-            glyphs = {
-              default = '',
-              symlink = '',
-              bookmark = '◉',
-              -- git = {
-              --   unstaged = '',
-              --   staged = '',
-              --   unmerged = '',
-              --   renamed = '',
-              --   deleted = '',
-              --   untracked = '',
-              --   ignored = '',
-              -- },
-              folder = {
-                default = '',
-                open = '',
-                symlink = '',
-              },
-            },
             show = {
               git = true,
               file = true,
@@ -61,7 +61,7 @@ return {
             enable = true,
           },
         },
-      })
+      }
     end,
   },
 }
--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -9,7 +9,7 @@ map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
-map('n', '<leader>e', ':CHADopen<CR>')
+map('n', '<leader>e', ':NvimTreeToggle<CR> <BAR> <S-e>')
 map('n', '<C-t>', ':tabnew<CR>')
 map('n', '<C-j>', '<C-w>j', { desc = 'Move Between splits with HJKL' })
 map('n', '<C-k>', '<C-w>k', { desc = 'Move Between splits with HJKL' })
