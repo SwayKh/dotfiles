4ffabbe3153833ed561e79221de58bd66af60780
SwayKh <khatrisumit712@gmail.com>
2023-11-07 00:17:07 +0530

    Update formatting

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,14 +1,13 @@
-
-return{
-        -- Set lualine as statusline
-        'nvim-lualine/lualine.nvim',
-        -- See `:help lualine.txt`
-        opts = {
-                options = {
-                        icons_enabled = false,
-                        theme = 'auto',
-                        component_separators = '|',
-                        section_separators = '',
-                },
-        },
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'auto',
+      component_separators = { left = '•', right = '•' }, --  ·•
+      section_separators = { left = '', right = '' }, -- 
+    },
+  },
 }
