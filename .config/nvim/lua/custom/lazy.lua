68111f79e39dfba9d17f4de3e0780fcbd16e9ee5
SwayKh <khatrisumit712@gmail.com>
2023-11-07 19:37:00 +0530

    Install various theme

--- .config/nvim/lua/custom/lazy.lua
+++ .config/nvim/lua/custom/lazy.lua
@@ -1,18 +1,25 @@
 require('lazy').setup({
   -- NOTE:First, some plugins that don't require any configuration
 
-  { "ThePrimeagen/vim-be-good" },
-  {"ms-jpq/chadtree"},
-  {"nvim-tree/nvim-web-devicons"},
-  {"lmburns/lf.nvim"},
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  { 'ThePrimeagen/vim-be-good' },
+  { 'ms-jpq/chadtree' },
+  { 'nvim-tree/nvim-web-devicons' },
+  { 'rcarriga/nvim-notify' },
+  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+  { 'tanvirtin/monokai.nvim' },
+  { 'navarasu/onedark.nvim' },
+  { 'sainnhe/gruvbox-material' },
+  { 'ellisonleao/gruvbox.nvim' },
+  { 'shaunsingh/nord.nvim' },
+  { 'rebelot/kanagawa.nvim' },
+  { 'Mofiqul/dracula.nvim' },
   {
     -- Theme inspired by Atom
-    "tanvirtin/monokai.nvim",
+    'tanvirtin/monokai.nvim',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'monokai'
-      vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
     end,
   },
