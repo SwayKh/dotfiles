2d293537d7150877d06281315e84a88d7e4f4dfb
SwayKh <khatrisumit712@gmail.com>
2023-11-07 20:32:54 +0530

    Add catpuccin theme

--- .config/nvim/lua/custom/lazy.lua
+++ .config/nvim/lua/custom/lazy.lua
@@ -17,13 +17,8 @@ require('lazy').setup({
     -- Theme inspired by Atom
     'tanvirtin/monokai.nvim',
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'monokai'
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-    end,
   },
-
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
