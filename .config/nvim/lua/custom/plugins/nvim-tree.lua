d14ffcea3ca7382b709bd08108bee5c80dc8b919
SwayKh <khatrisumit712@gmail.com>
2023-11-08 23:52:29 +0530

    Fix Nvim-tree, now shows dotfiles on startup

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,40 +1,37 @@
 return {
   {
-    'nvim-tree/nvim-tree.lua',
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
+
       local function my_on_attach(bufnr)
-        local api = require 'nvim-tree.api'
+        local api = require("nvim-tree.api")
 
         local function opts(desc)
-          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
         end
 
         -- default mappings
         api.config.mappings.default_on_attach(bufnr)
 
         -- custom mappings
-        vim.keymap.set('n', 'b', api.tree.change_root_to_node, opts 'Up')
-        vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+        vim.keymap.set("n", "b", api.tree.change_root_to_node, opts("Up"))
+        vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
       end
-      require('nvim-tree').setup()
+      require("nvim-tree").setup()
 
-      require('nvim-tree').setup {
+      require("nvim-tree").setup({
         on_attach = my_on_attach,
-        sort_by = 'case_sensitive',
-        open_on_tab = true,
-        disable_netrw = true,
-        hijack_netrw = true,
-        update_cwd = true,
-        hijack_cursor = true,
+        set_expand_all = function()
+          local api = require("nvim-tree.api")
+          api.tree.expand_all()
+        end,
         view = {
           width = 30,
           number = true,
           relativenumber = true,
-        },
-        filters = {
-          dotfiles = true,
         },
         git = {
           ignore = false,
@@ -45,14 +42,14 @@ return {
             resize_window = true,
           },
           expand_all = {
-            -- max_folder_discovery = 300,
-            exclude = { '.git', 'target', 'build' },
+            max_folder_discovery = 300,
+            exclude = { ".git", "target", "build" },
           },
         },
         renderer = {
           group_empty = true,
           highlight_git = true,
-          root_folder_modifier = ':t',
+          root_folder_modifier = ":t",
           icons = {
             show = {
               git = true,
@@ -65,7 +62,7 @@ return {
             enable = true,
           },
         },
-      }
+      })
     end,
   },
 }
