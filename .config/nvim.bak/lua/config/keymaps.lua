4d3b88cf62b4dcd837c845d20071a0baee43952b
SwayKh <khatrisumit712@gmail.com>
2023-12-13 20:58:46 +0530

    Remove nvim.bak finally

--- .config/nvim.bak/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-local map = vim.keymap.set
-
-map("n", "<C-d>", "<C-d>zz", { desc = "Better half down scroll", remap = true })
-map("n", "<C-u>", "<C-u>zz", { desc = "Better half down scroll", remap = true })
-map("x", "<leader>p", '"_dP', { desc = "Don't replace paste register when replacing something" })
