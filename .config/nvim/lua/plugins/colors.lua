07649ca694eed222cc726fd0b663d84bbec5913c
SwayKh <khatrisumit712@gmail.com>
2023-12-14 20:43:24 +0530

    Removed unused plugins and colorscheme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,6 +1,5 @@
 return {
   {
-    --1
     "catppuccin/nvim",
     lazy = true,
     priority = 1000,
@@ -8,208 +7,151 @@ return {
     enabled = true,
   },
   {
-    --2
-    "ribru17/bamboo.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --3
-    "hoprr/calvera-dark.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --4
-    "daschw/leaf.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --5
-    "Yazeed1s/oh-lucy.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --6
-    "Matsuuu/pinkmare",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --7
-    "tanvirtin/monokai.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --8
-    "cocopon/iceberg.vim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --9
-    "dracula/vim",
-    lazy = true,
-    priority = 1000,
-    name = "dracula",
-    enabled = false,
-  },
-  {
-    --10
-    "EdenEast/nightfox.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --11
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --12
-    "folke/tokyonight.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --13
-    "jnurmine/zenburn",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --14
-    "junegunn/seoul256.vim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --15
-    "ku1ik/vim-monokai",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --16
-    "lifepillar/vim-solarized8",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --17
-    "marko-cerovac/material.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --18
-    "mhartington/oceanic-next",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --19
-    "navarasu/onedark.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --20
-    "nlknguyen/papercolor-theme",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-  },
-  {
-    --21
-    "nordtheme/vim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-    name = "nordtheme",
-  },
-  {
-    --22
-    "projekt0n/github-nvim-theme",
-    lazy = true,
-    enabled = false,
-    priority = 1000,
-  },
-  {
-    --23
-    "rebelot/kanagawa.nvim",
-    lazy = true,
-    enabled = false,
-    priority = 1000,
-  },
-  {
-    --24
-    "romainl/apprentice",
-    lazy = true,
-    enabled = false,
-    priority = 1000,
-  },
-  {
-    --25
-    "rose-pine/neovim",
-    lazy = true,
-    enabled = false,
-    priority = 1000,
-    name = "rose-pine",
-  },
-  {
-    --26
-    "sainnhe/everforest",
-    enabled = false,
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    --27
-    "sainnhe/gruvbox-material",
-    enabled = false,
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    --28
     "sainnhe/sonokai",
-    enabled = true,
     lazy = true,
     priority = 1000,
   },
-  {
-    --29
-    "tomasiser/vim-code-dark",
-    enabled = false,
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "dylanaraps/wal.vim",
-    enabled = true,
-    lazy = true,
-    priority = 1000,
-  },
+  --   {
+  --     "dylanaraps/wal.vim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "ribru17/bamboo.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "hoprr/calvera-dark.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "daschw/leaf.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "Yazeed1s/oh-lucy.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "Matsuuu/pinkmare",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "tanvirtin/monokai.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "cocopon/iceberg.vim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "dracula/vim",
+  --     lazy = true,
+  --     priority = 1000,
+  --     name = "dracula",
+  --   },
+  --   {
+  --     "EdenEast/nightfox.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "ellisonleao/gruvbox.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "folke/tokyonight.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "jnurmine/zenburn",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "junegunn/seoul256.vim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "ku1ik/vim-monokai",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "lifepillar/vim-solarized8",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "marko-cerovac/material.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "mhartington/oceanic-next",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "navarasu/onedark.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "nlknguyen/papercolor-theme",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "nordtheme/vim",
+  --     lazy = true,
+  --     priority = 1000,
+  --     name = "nordtheme",
+  --   },
+  --   {
+  --     "projekt0n/github-nvim-theme",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "rebelot/kanagawa.nvim",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "romainl/apprentice",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "rose-pine/neovim",
+  --     lazy = true,
+  --     priority = 1000,
+  --     name = "rose-pine",
+  --   },
+  --   {
+  --     "sainnhe/everforest",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "sainnhe/gruvbox-material",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
+  --   {
+  --     "tomasiser/vim-code-dark",
+  --     lazy = true,
+  --     priority = 1000,
+  --   },
 }
