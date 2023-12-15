c18586761fbafe449d447b838a30ce0e8d5f0863
SwayKh <khatrisumit712@gmail.com>
2023-12-15 16:39:07 +0530

    Moved all configuration files from .config to Top Level

--- linkdots.sh
+++ linkdots.sh
@@ -3,56 +3,56 @@
 path=$(pwd)
 
 # HOME Directory
-sudo ln -sfn "$path"/.config/.zshrc "$HOME"/.zshrc
-sudo ln -sfn "$path"/.config/.profile "$HOME"/.profile
-sudo ln -sfn "$path"/.config/.bashrc "$HOME"/.bashrc
-sudo ln -sfn "$path"/.config/starship.toml "$HOME"/.config/starship.toml
-sudo ln -Sfn "$path"/.config/settings.json "$HOME"/.config/Code/User/settings.json
-# sudo ln -sfn "$path"/.config/tmux/tmux.conf "$HOME"/.tmux.conf
+sudo ln -sfn "$path"/.zshrc "$HOME"/.zshrc
+sudo ln -sfn "$path"/.profile "$HOME"/.profile
+sudo ln -sfn "$path"/.bashrc "$HOME"/.bashrc
+sudo ln -sfn "$path"/starship.toml "$HOME"/.config/starship.toml
+sudo ln -Sfn "$path"/settings.json "$HOME"/.config/Code/User/settings.json
+# sudo ln -sfn "$path"/tmux/tmux.conf "$HOME"/.tmux.conf
 
 # .config Directory
 
 [ -d "$HOME"/.config/btop ] && rm -rf "$HOME"/.config/btop
-sudo ln -sfn "$path"/.config/btop "$HOME"/.config/
+sudo ln -sfn "$path"/btop "$HOME"/.config/
 
 [ -d "$HOME"/.config/lf ] && rm -rf "$HOME"/.config/lf
-sudo ln -sfn "$path"/.config/lf "$HOME"/.config/
+sudo ln -sfn "$path"/lf "$HOME"/.config/
 
 [ -d "$HOME"/.config/nvim ] && rm -rf "$HOME"/.config/nvim
-sudo ln -sfn "$path"/.config/nvim "$HOME"/.config/
+sudo ln -sfn "$path"/nvim "$HOME"/.config/
 
 [ -d "$HOME"/.config/micro ] && rm -rf "$HOME"/.config/micro
-sudo ln -sfn "$path"/.config/micro "$HOME"/.config/
+sudo ln -sfn "$path"/micro "$HOME"/.config/
 
 [ -d "$HOME"/.config/kitty ] && rm -rf "$HOME"/.config/kitty
-sudo ln -sfn "$path"/.config/kitty "$HOME"/.config/
+sudo ln -sfn "$path"/kitty "$HOME"/.config/
 
 [ -d "$HOME"/.config/neofetch ] && rm -rf "$HOME"/.config/neofetch
-sudo ln -sfn "$path"/.config/neofetch "$HOME"/.config/
+sudo ln -sfn "$path"/neofetch "$HOME"/.config/
 
 [ -d "$HOME"/.config/hypr ] && rm -rf "$HOME"/.config/hypr
-sudo ln -sfn "$path"/.config/hypr "$HOME"/.config/
+sudo ln -sfn "$path"/hypr "$HOME"/.config/
 
 [ -d "$HOME"/.config/waybar ] && rm -rf "$HOME"/.config/waybar
-sudo ln -sfn "$path"/.config/waybar "$HOME"/.config/
+sudo ln -sfn "$path"/waybar "$HOME"/.config/
 
 [ -d "$HOME"/.config/rofi ] && rm -rf "$HOME"/.config/rofi
-sudo ln -sfn "$path"/.config/rofi "$HOME"/.config/
+sudo ln -sfn "$path"/rofi "$HOME"/.config/
 
 [ -d "$HOME"/.config/foot ] && rm -rf "$HOME"/.config/foot
-sudo ln -sfn "$path"/.config/foot "$HOME"/.config/
+sudo ln -sfn "$path"/foot "$HOME"/.config/
 
 [ -d "$HOME"/.config/dunst ] && rm -rf "$HOME"/.config/dunst
-sudo ln -sfn "$path"/.config/dunst "$HOME"/.config/
+sudo ln -sfn "$path"/dunst "$HOME"/.config/
 
 [ -d "$HOME"/.config/tmux ] && rm -rf "$HOME"/.config/tmux
-sudo ln -sfn "$path"/.config/tmux "$HOME"/.config/
+sudo ln -sfn "$path"/tmux "$HOME"/.config/
 
 [ -d "$HOME"/.config/swaylock ] && rm -rf "$HOME"/.config/swaylock
-sudo ln -sfn "$path"/.config/swaylock "$HOME"/.config/
+sudo ln -sfn "$path"/swaylock "$HOME"/.config/
 
 [ -d "$HOME"/.config/swayidle ] && rm -rf "$HOME"/.config/swayidle
-sudo ln -sfn "$path"/.config/swayidle "$HOME"/.config/
+sudo ln -sfn "$path"/swayidle "$HOME"/.config/
 
 [ -f "$HOME"/.config/retroarch/retroarch.cfg ] && rm $HOME/.config/retroarch/retroarch.cfg
-sudo ln -sfn "$path"/.config/retroarch.cfg "$HOME"/.config/retroarch/retroarch.cfg
+sudo ln -sfn "$path"/retroarch.cfg "$HOME"/.config/retroarch/retroarch.cfg
