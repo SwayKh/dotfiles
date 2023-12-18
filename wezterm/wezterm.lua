9f7b7e78d9702b1d85b27702593ff0ca3a0699ef
SwayKh <khatrisumit712@gmail.com>
2023-12-18 20:43:50 +0530

    Add wezterm, using xwayland since there's open issue on hyprland

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require("wezterm")
+return {
+	-- color_scheme = 'termnial.sexy',
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	font_size = 12.0,
+	enable_wayland = false,
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+
+	-- macos_window_background_blur = 40,
+	macos_window_background_blur = 30,
+
+	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
+	-- window_background_image_hsb = {
+	-- 	brightness = 0.01,
+	-- 	hue = 1.0,
+	-- 	saturation = 0.5,
+	-- },
+	-- window_background_opacity = 0.92,
+	window_background_opacity = 0.6,
+	-- window_background_opacity = 0.78,
+	-- window_background_opacity = 0.20,
+	window_decorations = "RESIZE",
+	keys = {
+		{
+			key = "f",
+			mods = "CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+}
