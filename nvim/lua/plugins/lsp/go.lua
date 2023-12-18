64e48ce4886f884428aecca81c2afc5d7ca4d87e
SwayKh <khatrisumit712@gmail.com>
2023-12-18 18:43:51 +0530

    Add ray-x/go.nvim plugin

---
+++ nvim/lua/plugins/lsp/go.lua
@@ -0,0 +1,24 @@
+return {
+  "ray-x/go.nvim",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
+
+    -- Run gofmt + goimport on save
+    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require("go.format").goimport()
+      end,
+      group = format_sync_grp,
+    })
+  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+}
