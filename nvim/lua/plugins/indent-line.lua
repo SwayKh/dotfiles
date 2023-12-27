1ca4860d9c8e54f70164e34a1c4eaeb97cee3083
SwayKh <khatrisumit712@gmail.com>
2023-12-27 16:58:01 +0530

    Add separate configs for comment and indent-line, disable git plugins in init.lua

---
+++ nvim/lua/plugins/indent-line.lua
@@ -0,0 +1,29 @@
+return {
+  -- Add indentation guides even on blank lines
+  "lukas-reineke/indent-blankline.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+  main = "ibl",
+}
