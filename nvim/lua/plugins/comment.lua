b7361c6e63b6bf3719325aff4efe431ec45ff2fd
SwayKh <khatrisumit712@gmail.com>
2023-12-27 16:58:01 +0530

    Add separate configs for comment and indent-line, disable git plugins in init.lua

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,23 @@
+return {
+  "numToStr/Comment.nvim",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+  opts = {},
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      sticky = true,
+      padding = true,
+      toggler = {
+        line = "<M-/>",
+      },
+      opleader = {
+        line = "<M-/>",
+      },
+      mappings = {
+        basic = true,
+        extra = true,
+      },
+    })
+  end,
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,64 +5,13 @@ return {
   --   event = { "BufReadPre", "BufNewFile" },
   -- },
   { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "tpope/vim-fugitive", lazy = false },
-  { "tpope/vim-rhubarb", lazy = false },
-  { "tpope/vim-sleuth", lazy = false },
-  { "christoomey/vim-tmux-navigator", lazy = false },
   { "NvChad/nvim-colorizer.lua", config = true },
-  { "kevinhwang91/nvim-bqf" },
-  {
-    -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      indent = {
-        char = "│",
-        tab_char = "│",
-      },
-      scope = { enabled = false },
-      exclude = {
-        filetypes = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-      },
-    },
-    main = "ibl",
-  },
-  {
-    "numToStr/Comment.nvim",
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    opts = {},
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("Comment").setup({
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-        sticky = true,
-        padding = true,
-        toggler = {
-          line = "<M-/>",
-        },
-        opleader = {
-          line = "<M-/>",
-        },
-        mappings = {
-          basic = true,
-          extra = true,
-        },
-      })
-    end,
-  },
+  { "christoomey/vim-tmux-navigator", lazy = false },
+
+  { "tpope/vim-fugitive", lazy = false, enabled = false },
+  { "tpope/vim-rhubarb", lazy = false, enabled = false },
+  { "tpope/vim-sleuth", lazy = false, enabled = false },
+  { "kevinhwang91/nvim-bqf", lazy = true, enabled = false },
   {
     "kylechui/nvim-surround",
     enabled = false,
