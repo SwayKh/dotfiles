28ee85bf9b2b4cfec2c035c71f189550572944cd
SwayKh <khatrisumit712@gmail.com>
2024-01-18 19:35:15 +0530

    Disable some plugins

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  enabled = false,
   opts = {},
   config = function()
     require("gitsigns").setup({
