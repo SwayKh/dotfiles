e2e054dadaffb6d2a125dfdd9451dd90ad83cc49
SwayKh <khatrisumit712@gmail.com>
2024-01-30 19:04:36 +0530

    Add autocmd for opening files to their last edited position

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -40,6 +40,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+            vim.api.nvim_win_set_cursor(0, mark)
+        end
+    end,
+})
+
 -- local function theme()
 -- vim.cmd.colorscheme("catppuccin")
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
