ad686a1a04e8bdd0c0626c326233be5ec74c3422
SwayKh <khatrisumit712@gmail.com>
2024-01-30 19:05:58 +0530

    Remove ts-textobject dependency, and its config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,10 @@
 return {
   -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
-  lazy = true,
-  event = { "BufReadPre", "BufNewFile" },
+  -- lazy = true,
+  -- event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      event = { "BufReadPre", "BufNewFile" },
-    },
     {
       "windwp/nvim-ts-autotag",
       event = { "BufReadPre", "BufNewFile" },
@@ -60,50 +56,6 @@ return {
           node_incremental = "<c-space>",
           scope_incremental = "<c-s>",
           node_decremental = "<M-space>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-          },
         },
       },
     })
