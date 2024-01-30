0ef7787d30d21749aa4eedf0f34294902093a8f1
SwayKh <khatrisumit712@gmail.com>
2024-01-30 19:07:40 +0530

    Move disabled plugins for init to disabled/misc.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,30 +5,5 @@ return {
   --   event = { "BufReadPre", "BufNewFile" },
   -- },
   { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "christoomey/vim-tmux-navigator", lazy = false },
-
-  { "tpope/vim-fugitive", lazy = false, enabled = false },
-  { "tpope/vim-rhubarb", lazy = false, enabled = false },
-  { "tpope/vim-sleuth", lazy = false, enabled = false },
-  { "kevinhwang91/nvim-bqf", lazy = true, enabled = false },
-  {
-    "kylechui/nvim-surround",
-    enabled = false,
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    lazy = true,
-    event = "InsertEnter",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  },
-  {
-    "karb94/neoscroll.nvim",
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("neoscroll").setup({})
-    end,
-  },
+  { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
 }
