dfc425981a81cae35394cfe10ca102c185bcf35c
SwayKh <khatrisumit712@gmail.com>
2024-02-13 14:50:18 +0530

    Add a Redir user command, to get output of cmd commands to tmp buffer

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -68,6 +68,12 @@ vim.api.nvim_create_autocmd("RecordingLeave", {
     )
   end,
 })
+vim.api.nvim_create_user_command("Redir", function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), "\n", { plain = true })
+  vim.cmd("new")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
 
 -- vim.api.nvim_create_autocmd("VimEnter", {
 --   callback = vim.schedule_wrap(function()
