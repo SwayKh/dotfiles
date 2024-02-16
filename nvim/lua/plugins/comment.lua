72dafe3ce0564201fcf07c57b0c7f121a64f84c7
SwayKh <khatrisumit712@gmail.com>
2024-02-16 22:11:33 +0530

    Misc changes, too small for individual commit
    comment.nvim starts treesitter, with treesitter-comment-string
    fzflua position changes
    lualine components color update to create a "flow"
    nvColorizer, add lazy loading
    Add vcs icons to oil with new plugins

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,6 @@
 return {
   "numToStr/Comment.nvim",
   dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-  opts = {},
   lazy = true,
   event = "VeryLazy",
   -- event = { "BufReadPre", "BufNewFile" },
--- nvim/lua/plugins/nvColorizer.lua
+++ nvim/lua/plugins/nvColorizer.lua
@@ -1,5 +1,7 @@
 return {
   "NvChad/nvim-colorizer.lua",
+  lazy = true,
+  event = "VeryLazy",
   config = function()
     require("colorizer").setup({
       filetype = { "*" },
