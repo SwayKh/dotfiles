1d7f86a9698eb968b286ca2cb68e3496879c7253
SwayKh <khatrisumit712@gmail.com>
2024-02-17 22:01:42 +0530

    Change theme to edge, Update formatting in whole file

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,11 +1,10 @@
 return {
   {
-    "dracula/vim",
-    name = "dracula",
+    "sainnhe/edge",
     priority = 1000,
     enabled = true,
     config = function()
-      vim.cmd.colorscheme("dracula")
+      vim.cmd.colorscheme("edge")
       vim.opt.background = "dark"
       vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
       vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
@@ -14,98 +13,102 @@ return {
       -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
     end,
   },
-  --    {
-  --      "catppuccin/nvim",
-  --      name = "catppuccin"
-  --    }
-  --    {
-  --      "sainnhe/sonokai",
-  --    },
-  --   {
-  --     "dylanaraps/wal.vim",
-  --   },
-  --   {
-  --     "ribru17/bamboo.nvim",
-  --   },
-  --   {
-  --     "hoprr/calvera-dark.nvim",
-  --   },
-  --   {
-  --     "daschw/leaf.nvim",
-  --   },
-  --   {
-  --     "Yazeed1s/oh-lucy.nvim",
-  --   },
-  --   {
-  --     "Matsuuu/pinkmare",
-  --   },
-  --   {
-  --     "tanvirtin/monokai.nvim",
-  --   },
-  --   {
-  --     "cocopon/iceberg.vim",
-  --   },
-  --   {
-  --     "dracula/vim",
-  --     name = "dracula",
-  --   },
-  --   {
-  --     "EdenEast/nightfox.nvim",
-  --   },
-  --   {
-  --     "ellisonleao/gruvbox.nvim",
-  --   },
-  --   {
-  --     "folke/tokyonight.nvim",
-  --   },
-  --   {
-  --     "jnurmine/zenburn",
-  --   },
-  --   {
-  --     "junegunn/seoul256.vim",
-  --   },
-  --   {
-  --     "ku1ik/vim-monokai",
-  --   },
-  --   {
-  --     "lifepillar/vim-solarized",
-  --   },
-  --   {
-  --     "marko-cerovac/material.nvim",
-  --   },
-  --   {
-  --     "mhartington/oceanic-next",
-  --   },
-  --   {
-  --     "navarasu/onedark.nvim",
-  --   },
-  --   {
-  --     "nlknguyen/papercolor-theme",
-  --   },
-  --   {
-  --     "nordtheme/vim",
-  --     name = "nord",
-  --   },
-  --   {
-  --     "projekt0n/github-nvim-theme",
-  --   },
-  --   {
-  --     "rebelot/kanagawa.nvim",
-  --   },
-  --   {
-  --     "romainl/apprentice",
-  --   },
-  --   {
-  --     "rose-pine/neovim",
-  --     name = "rose-pine",
-  --   },
-  --   {
-  --     "sainnhe/everforest",
-  --   },
-  --   {
-  --     "sainnhe/gruvbox-material",
-  --   },
-  --   {
-  --     "tomasiser/vim-code-dark",
-  --   },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  -- },
+  -- {
+  --   "sainnhe/sonokai",
+  -- },
+  -- {
+  --   "dracula/vim",
+  --   name = "dracula",
+  -- },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  -- },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  -- },
+  -- {
+  --   "sainnhe/edge",
+  -- },
+  -- {
+  --   "navarasu/onedark.nvim",
+  -- },
+  --
+  -- {
+  --   "dylanaraps/wal.vim",
+  -- },
+  -- {
+  --   "ribru17/bamboo.nvim",
+  -- },
+  -- {
+  --   "hoprr/calvera-dark.nvim",
+  -- },
+  -- {
+  --   "daschw/leaf.nvim",
+  -- },
+  -- {
+  --   "Yazeed1s/oh-lucy.nvim",
+  -- },
+  -- {
+  --   "Matsuuu/pinkmare",
+  -- },
+  -- {
+  --   "tanvirtin/monokai.nvim",
+  -- },
+  -- {
+  --   "cocopon/iceberg.vim",
+  -- },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  -- },
+  -- {
+  --   "jnurmine/zenburn",
+  -- },
+  -- {
+  --   "junegunn/seoul256.vim",
+  -- },
+  -- {
+  --   "ku1ik/vim-monokai",
+  -- },
+  -- {
+  --   "lifepillar/vim-solarized",
+  -- },
+  -- {
+  --   "marko-cerovac/material.nvim",
+  -- },
+  -- {
+  --   "mhartington/oceanic-next",
+  -- },
+  -- {
+  --   "nlknguyen/papercolor-theme",
+  -- },
+  -- {
+  --   "nordtheme/vim",
+  --   name = "nord",
+  -- },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  -- },
+  -- {
+  --   "romainl/apprentice",
+  -- },
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  -- },
+  -- {
+  --   "sainnhe/everforest",
+  -- },
+  -- {
+  --   "sainnhe/gruvbox-material",
+  -- },
+  -- {
+  --   "tomasiser/vim-code-dark",
+  -- },
 }
