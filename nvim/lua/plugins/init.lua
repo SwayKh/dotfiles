6bf91320f75037044596bb53bd0930455eab7e9d
SwayKh <khatrisumit712@gmail.com>
2024-03-05 22:50:11 +0530

    Remove Telescope as first source for dressing, remove vim-tmux-navigator plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,6 +4,8 @@ return {
   --   lazy = true,
   --   event = { "BufReadPre", "BufNewFile" },
   -- },
+  --
+  { "tpope/vim-sleuth", event = "VeryLazy" }, -- Detect tabstop and shiftwidth automatically
   { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
+  -- { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
 }
