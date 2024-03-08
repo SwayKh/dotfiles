e41b3e90d7d3d20d8b6e3b07556c200d5ea8cc84
SwayKh <khatrisumit712@gmail.com>
2024-03-08 14:51:16 +0530

    Add ray-x go.nvim back. With correct Lazy loading this time

--- nvim/lua/plugins/lsp/go.lua
+++ nvim/lua/plugins/lsp/go.lua
@@ -1,7 +1,8 @@
 return {
   "ray-x/go.nvim",
-  event = "VeryLazy",
-  enabled = false,
+  lazy = true,
+  ft = { "go", "gomod", "gowork", "gotmpl" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   dependencies = { -- optional packages
     "ray-x/guihua.lua",
     "neovim/nvim-lspconfig",
@@ -20,7 +21,4 @@ return {
       group = format_sync_grp,
     })
   end,
-  -- event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
-  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
