b9a4519bc9a3a3059a5785cd198deeff3092f45e
SwayKh <khatrisumit712@gmail.com>
2024-03-08 21:26:24 +0530

    Move around some keybinds

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -4,19 +4,32 @@ local map = vim.keymap.set
 
 -- My Keybinds
 -- Plugin specific keymaps are in the plugin configs
+-- map("i", "jj", "<Esc>")
+
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-map("n", "<Esc>", "<Cmd>nohlsearch<CR>", { silent = true })
-map("i", "jj", "<Esc>")
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 map("n", "<leader>m", "@@", { silent = true, desc = "Run last [M]acro, @@ is hard to do" })
+
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "<C-u>", "<C-u>zz", { desc = "Better half up scroll", remap = true })
+map("n", "<C-d>", "<C-d>zz", { desc = "Better half down scroll", remap = true })
+
+map("n", "<leader>V", "<Cmd>vsplit<CR>", { desc = "Split [V]ertically" })
+map("n", "<leader>S", "<Cmd>split<CR>", { desc = "[S]plit Horizontally (Default behaviour)" })
 
 map("n", "<leader>y", "<Cmd>%y+<CR>", { silent = true, desc = "Copy whole file" })
 map("x", "<leader>p", '"_dP', { silent = true, desc = "Don't replace paste register when replacing something" })
 
 map("n", "qq", "<Cmd>q!<CR>", { silent = true, desc = "[Q]uit" })
 map("n", "<leader>bd", "<Cmd>bd<CR>", { silent = true, desc = "Run [B]buffer [D]elete" })
-map({ "n", "i", "v" }, "<C-q>", "<ESC><Cmd>wqa!<CR>", { silent = true, desc = "Save all files [Q]uit" })
 map({ "n", "i", "v" }, "<C-s>", "<ESC><Cmd>w!<CR>", { silent = true, desc = "[S]ave file" })
+map({ "n", "i", "v" }, "<C-q>", "<ESC><Cmd>wqa!<CR>", { silent = true, desc = "Save all files [Q]uit" })
 
+map("n", "<leader>;l", "<Cmd>Lazy<CR>", { silent = true, desc = "Open [L]azy" })
+map("n", "<leader>;m", "<Cmd>Mason<CR>", { silent = true, desc = "Open [M]ason" })
 map("n", "<leader>lr", ":Lazy reload", { desc = "[L]azy [R]eload plugin of choice" })
 map("n", "-", "<Cmd>Oil --float<CR>", { silent = true, desc = "Open Oil in floating mode" })
 
@@ -36,14 +49,6 @@ map(
   { silent = true, desc = "Source nvim config, and reload all plugins" }
 )
 
-map("n", "<C-u>", "<C-u>zz", { desc = "Better half up scroll", remap = true })
-map("n", "<C-d>", "<C-d>zz", { desc = "Better half down scroll", remap = true })
-
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 map("i", "<C-j>", "<Down>", { desc = "Move Down in insert mode" })
 map("i", "<C-k>", "<Up>", { desc = "Move Up in insert mode" })
 map("i", "<C-h>", "<Left>", { desc = "Move Left in insert mode" })
@@ -55,25 +60,12 @@ map("n", "<C-h>", "<C-w>h", { desc = "Move Between splits with HJKL" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move Between splits with HJKL" })
 
 -- Split Resize
-map("n", "<leader>V", "<Cmd>vsplit<CR>", { desc = "Split [V]ertically" })
-map("n", "<leader>S", "<Cmd>split<CR>", { desc = "[S]plit Horizontally (Default behaviour)" })
-map("n", "<C-S-Left>", "<Cmd>vertical resize +3<CR>") -- Control+Left resizes vertical split +
-map("n", "<C-S-Right>", "<Cmd>vertical resize -3<CR>") -- Control+Right resizes vertical split -
-
 map("n", "<C-S-h>", "<C-w>>") -- resize window to left
 map("n", "<C-S-l>", "<C-w><") -- resize window to right
 map("n", "<C-S-k>", "<C-w>+") -- resize window to up
 map("n", "<C-S-j>", "<C-w>-") -- resize window to down
-
--- Autocomplete brackets and quotes
--- Feels annoying somehow, autopair plugin is better
--- map("i", "'", "''<left>")
--- map("i", '"', '""<left>')
--- map("i", "(", "()<left>")
--- map("i", "[", "[]<left>")
--- map("i", "{", "{}<left>")
--- map("i", "{;", "{};<left><left>")
--- map("i", "/*", "/**/<left><left>")
+map("n", "<C-S-Left>", "<Cmd>vertical resize +3<CR>") -- Control+Left resizes vertical split +
+map("n", "<C-S-Right>", "<Cmd>vertical resize -3<CR>") -- Control+Right resizes vertical split -
 
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
@@ -88,3 +80,13 @@ vim.cmd("cnoreabbrev Qa qa")
 vim.cmd("cnoreabbrev W  w")
 vim.cmd("cnoreabbrev Wq wq")
 vim.cmd("cnoreabbrev WQ wq")
+
+-- Autocomplete brackets and quotes
+-- Feels annoying somehow, autopair plugin is better
+-- map("i", "'", "''<left>")
+-- map("i", '"', '""<left>')
+-- map("i", "(", "()<left>")
+-- map("i", "[", "[]<left>")
+-- map("i", "{", "{}<left>")
+-- map("i", "{;", "{};<left><left>")
+-- map("i", "/*", "/**/<left><left>")
