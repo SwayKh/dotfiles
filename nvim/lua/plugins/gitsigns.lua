e3b73a21cce148c31709aa9793109175bcca7257
SwayKh <khatrisumit712@gmail.com>
2024-03-09 16:42:39 +0530

    Load flash and gitsign on VeryLazy event, Too many BufPreRead plugins

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  event = "VeryLazy",
   opts = {},
   config = function()
     require("gitsigns").setup({
