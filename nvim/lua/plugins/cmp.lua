f6294862ca498def4d97aaf5713223d77c42c5e1
SwayKh <khatrisumit712@gmail.com>
2024-03-12 19:52:04 +0530

    Use C-y to accept cmp completion(Can't go to newline with cmp window with enter)

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -47,7 +47,8 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         -- ["<Tab>"] = cmp.mapping(function(fallback)
         --   if cmp.visible() then
         --     cmp.select_next_item()
