072fbfe5606b50c18f922f858d0f8c5a30c66301
SwayKh <khatrisumit712@gmail.com>
2024-04-09 22:32:24 +0530

    Remove alpha dashboard

--- nvim/lua/plugins/disabled/alpha.lua
+++ nvim/lua/plugins/disabled/alpha.lua
@@ -3,8 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "VimEnter", -- Sets the loading event to 'VimEnter'
   config = function()
-    require("alpha").setup(require("alpha.themes.dashboard").config)
-    local alpha = require("alpha")
+    require("lua.plugins.disabled.alpha").setup(require("alpha.themes.dashboard").config)
+    local alpha = require("lua.plugins.disabled.alpha")
     local dashboard = require("alpha.themes.dashboard")
 
     -- Set header
