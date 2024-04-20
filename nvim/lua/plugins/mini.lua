2a2fc8524c2690ea37a7c0290df269574e272a4e
SwayKh <khatrisumit712@gmail.com>
2024-04-20 17:27:48 +0530

    Add mini.surround, and replace flash keybind to m,

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -20,7 +20,20 @@ return { -- Collection of various small independent plugins/modules
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
     --  [s/S] keybinds are used for flash, will figure it out
-    -- require("mini.surround").setup()
+    require("mini.surround").setup({
+      mappings = {
+        add = "sa", -- Add surrounding in Normal and Visual modes
+        delete = "sd", -- Delete surrounding
+        find = "sf", -- Find surrounding (to the right)
+        find_left = "sF", -- Find surrounding (to the left)
+        highlight = "sh", -- Highlight surrounding
+        replace = "sr", -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
+    })
 
     -- require("mini.starter").setup() -- Won't work because mini is lazy loaded
     -- require("mini.indentscope").setup()
