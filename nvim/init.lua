0ce771ae3befc1ae2065a4aabdffe249119c9b33
SwayKh <khatrisumit712@gmail.com>
2024-04-23 13:33:23 +0530

    Update loading order of configs, Don't need termguicolor in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 --Need this to be loaded before some plugins, but option.lua needs colorscheme to be loaded
-vim.opt.termguicolors = true
+-- vim.opt.termguicolors = true
 
-require("config.lazy")
-require("config.keybinds")
 require("config.option")
+require("config.keybinds")
 require("config.misc")
+require("config.lazy")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
