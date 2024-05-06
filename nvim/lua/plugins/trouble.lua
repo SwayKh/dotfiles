01fc7768689007766143b223ae20bc92f16d7468
SwayKh <khatrisumit712@gmail.com>
2024-05-06 13:52:28 +0530

    Add Trouble.nvim plugin

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
