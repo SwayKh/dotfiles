571ad3742c661521666506ede1d0f9d82882f821
SwayKh <khatrisumit712@gmail.com>
2024-05-23 15:21:26 +0530

    Add new nvim plugin precognition, Useful with hardtime.nvim

---
+++ nvim/lua/plugins/precognition.lua
@@ -0,0 +1,30 @@
+return {
+  "tris203/precognition.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("precognition").setup({
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = "Comment",
+      hints = {
+        Caret = { text = "^", prio = 2 },
+        Dollar = { text = "$", prio = 1 },
+        MatchingPair = { text = "%", prio = 5 },
+        Zero = { text = "0", prio = 1 },
+        w = { text = "w", prio = 10 },
+        b = { text = "b", prio = 9 },
+        e = { text = "e", prio = 8 },
+        W = { text = "W", prio = 7 },
+        B = { text = "B", prio = 6 },
+        E = { text = "E", prio = 5 },
+      },
+      gutterHints = {
+        -- prio is not currently used for gutter hints
+        G = { text = "G", prio = 1 },
+        gg = { text = "gg", prio = 1 },
+        PrevParagraph = { text = "{", prio = 1 },
+        NextParagraph = { text = "}", prio = 1 },
+      },
+    })
+  end,
+}
