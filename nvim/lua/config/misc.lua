0beef78a1533a390805c1eccb0684d4918dd91d6
SwayKh <khatrisumit712@gmail.com>
2024-05-24 17:04:18 +0530

    Update Mini.Statusline with new default config. Disabled precognition.nvim

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -15,8 +15,8 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = { "*.txt" },
   callback = function()
     if vim.o.filetype == "help" then
-      vim.cmd([[ 
-        wincmd L  
+      vim.cmd([[
+        wincmd L
         vertical resize 90
       ]])
     end
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -118,12 +118,32 @@ return { -- Collection of various small independent plugins/modules
       },
     })
 
-    local statusline = require("mini.statusline")
-    statusline.setup({ use_icons = vim.g.have_nerd_font })
+    require("mini.statusline").setup({
+      use_icons = vim.g.have_nerd_font,
 
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return "%2l:%-2v"
-    end
+      content = {
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          -- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local location = MiniStatusline.section_location({ trunc_width = 200 })
+          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+            "%<", -- Mark general truncate point
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=", -- End left alignment
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          })
+        end,
+      },
+    })
   end,
 }
