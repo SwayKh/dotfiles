66533187f84163feb36ccc148dda5a6b5b7506ef
SwayKh <khatrisumit712@gmail.com>
2024-06-06 23:01:14 +0530

    Add Mini Animate and Bracketed

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -71,6 +71,29 @@ return { -- Collection of various small independent plugins/modules
     require("mini.notify").setup()
     require("mini.git").setup()
 
+    require("mini.animate").setup({
+      cursor = {
+        enable = false,
+      },
+    })
+
+    require("mini.bracketed").setup({
+      buffer = { suffix = "b", options = {} },
+      comment = { suffix = "c", options = {} },
+      conflict = { suffix = "x", options = {} },
+      diagnostic = { suffix = "d", options = {} },
+      file = { suffix = "f", options = {} },
+      indent = { suffix = "i", options = {} },
+      jump = { suffix = "j", options = {} },
+      location = { suffix = "l", options = {} },
+      oldfile = { suffix = "o", options = {} },
+      quickfix = { suffix = "q", options = {} },
+      treesitter = { suffix = "t", options = {} },
+      undo = { suffix = "u", options = {} },
+      window = { suffix = "w", options = {} },
+      yank = { suffix = "y", options = {} },
+    })
+
     -- Comment lines in visual or normal mode
     -- Nvim 10.0 has commenting builtin with these keybinds
     -- require("mini.comment").setup({
@@ -145,5 +168,49 @@ return { -- Collection of various small independent plugins/modules
         end,
       },
     })
+
+    -- require("mini.clue").setup({
+    --   triggers = {
+    --     -- Leader triggers
+    --     { mode = "n", keys = "<Leader>" },
+    --     { mode = "x", keys = "<Leader>" },
+    --
+    --     -- Built-in completion
+    --     { mode = "i", keys = "<C-x>" },
+    --
+    --     -- `g` key
+    --     { mode = "n", keys = "g" },
+    --     { mode = "x", keys = "g" },
+    --
+    --     -- Marks
+    --     { mode = "n", keys = "'" },
+    --     { mode = "n", keys = "`" },
+    --     { mode = "x", keys = "'" },
+    --     { mode = "x", keys = "`" },
+    --
+    --     -- Registers
+    --     { mode = "n", keys = '"' },
+    --     { mode = "x", keys = '"' },
+    --     { mode = "i", keys = "<C-r>" },
+    --     { mode = "c", keys = "<C-r>" },
+    --
+    --     -- Window commands
+    --     { mode = "n", keys = "<C-w>" },
+    --
+    --     -- `z` key
+    --     { mode = "n", keys = "z" },
+    --     { mode = "x", keys = "z" },
+    --   },
+    --
+    --   clues = {
+    --     -- Enhance this by adding descriptions for <Leader> mapping groups
+    --     require("mini.clue").gen_clues.builtin_completion(),
+    --     require("mini.clue").gen_clues.g(),
+    --     require("mini.clue").gen_clues.marks(),
+    --     require("mini.clue").gen_clues.registers(),
+    --     require("mini.clue").gen_clues.windows(),
+    --     require("mini.clue").gen_clues.z(),
+    --   },
+    -- })
   end,
 }
