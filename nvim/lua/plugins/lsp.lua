9665831daeda68bea37a25b9d418e6138d0b58a7
SwayKh <khatrisumit712@gmail.com>
2024-06-12 23:05:29 +0530

    Add build command to LuaSnip plugin and Load Mason before others plugins in neovim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for neovim
-    "williamboman/mason.nvim",
+    { "williamboman/mason.nvim", config = true },
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     "hrsh7th/cmp-nvim-lsp", -- for autocompletion
