6b04c332c7522f59e98f8ce93a08f0fe17469bbb
SwayKh <khatrisumit712@gmail.com>
2024-06-16 23:18:23 +0530

    Remove codeium for supermaven AI plugin in nvim

---
+++ nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  event = "VeryLazy",
+  config = function()
+    require("supermaven-nvim").setup({
+      disable_inline_completion = true, -- disables inline completion for use with cmp
+      disable_keymaps = true, -- disables built in keymaps for more manual control
+    })
+  end,
+}
