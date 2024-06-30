b715998ac441ac5aaee726573b55edf88580cb78
SwayKh <khatrisumit712@gmail.com>
2024-06-30 22:51:42 +0530

    Remove which-key, replaced with mini.clues in neovim

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -123,6 +123,51 @@ return { -- Collection of various small independent plugins/modules
       },
     })
 
+    local miniclue = require("mini.clue")
+    miniclue.setup({
+      triggers = {
+        -- Leader triggers
+        { mode = "n", keys = "<Leader>" },
+        { mode = "x", keys = "<Leader>" },
+
+        -- Built-in completion
+        { mode = "i", keys = "<C-x>" },
+
+        -- `g` key
+        { mode = "n", keys = "g" },
+        { mode = "x", keys = "g" },
+
+        -- Marks
+        { mode = "n", keys = "'" },
+        { mode = "n", keys = "`" },
+        { mode = "x", keys = "'" },
+        { mode = "x", keys = "`" },
+
+        -- Registers
+        { mode = "n", keys = '"' },
+        { mode = "x", keys = '"' },
+        { mode = "i", keys = "<C-r>" },
+        { mode = "c", keys = "<C-r>" },
+
+        -- Window commands
+        { mode = "n", keys = "<C-w>" },
+
+        -- `z` key
+        { mode = "n", keys = "z" },
+        { mode = "x", keys = "z" },
+      },
+
+      clues = {
+        -- Enhance this by adding descriptions for <Leader> mapping groups
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+      },
+    })
+
     require("mini.statusline").setup({
       use_icons = vim.g.have_nerd_font,
       content = {
