c65e66a9794daab89b0fb9083a04a6db4dbbb71d
SwayKh <khatrisumit712@gmail.com>
2024-07-30 15:46:51 +0530

    (Nvim) Add nvim-lint and Noice + notify nvim plugins

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,12 +1,19 @@
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+  event = { "VeryLazy", "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
 
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
-      python = { "pylint" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      python = { "pylint", "ruff" },
+      go = { "djlint", "golangcilint" },
+      bash = { "shellcheck" },
+      cpp = { "cpplint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
