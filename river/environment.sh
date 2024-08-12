9fe364f3c7271e8f94157fe4d1728ff4a84a3594
SwayKh <khatrisumit712@gmail.com>
2024-08-12 23:27:39 +0530

    (River) Add Export env variable script for river, Create temp scratchpad script

---
+++ river/environment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export XCURSOR_THEME=Bibata-Modern-Classic
+export XCURSOR_SIZE=18
+export XDG_CURRENT_DESKTOP=River
+export XDG_SESSION_DESKTOP=River
+export XDG_SESSION_TYPE=wayland
+export MOZ_ENABLE_WAYLAND=1
+export ELECTRON_OZONE_PLATFORM_HINT=wayland
+export BEMENU_BACKEND=wayland
+export CLUTTER_BACKEND=wayland
+export SDL_VIDEODRIVER=wayland
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export QT_SCALE_FACTOR=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export GDK_SCALE=1
+export GDK_BACKEND="wayland,x11,*"
