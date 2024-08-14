f5e002c31c5fbb78e65d0f8b0f31aedffdc1ae35
SwayKh <khatrisumit712@gmail.com>
2024-08-14 13:09:14 +0530

    (River) Finally got auto spawning scratchpads to work with scratchpad.sh script.
    Move rivertile keybinds to a resize mode
    Decrease keyboard repeat rate
    Remove sticky tags
    Move from foot to footclient
    Add color picker script
    Uncomment monitor/output binds
    Finally got rivertile to work, Had to put on the last line(I think)

--- river/scratchpads.sh
+++ river/scratchpads.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#
+# Riverctl script to open scratchpads, and open programs if Scratchpads are
+# empty. Usage "./scratchpads.sh <term|btop|lf|nvim>"
+#
 
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Error: No argument provided."
+  exit 1
+fi
+
+terminal="footclient"
 terminal_tag=$((1 << 11))
 lf_tag=$((1 << 12))
 dots_tag=$((1 << 13))
@@ -8,17 +19,34 @@ monitor_tag=$((1 << 14))
 term="$terminal --app-id Special_term -e tmux new-session -s Terminal"
 lf="$terminal --app-id Special_lf -e zsh -ic \"tmux new-session -s FileManager 'lf'\"" #lfMention
 btop="$terminal --app-id Special_btop -e btop"
-nvim="$terminal --app-id Special_nvim --working-directory=$HOME/dotfiles/ -e tmux new-session -s Editor 'nvim'"
+nvim="$terminal --app-id Special_nvim --working-directory=$HOME/dotfiles/ -e zsh -ic \"tmux new-session -s Editor 'nvim'\""
 
-# Check if the application is already running
-# if [ "$1" = "dots" ]; then
-#   # Toggle the scratchpad tag
-#   riverctl focus-output next
-#   riverctl set-focused-tags 11
-# else
-#   # Launch the application and send it to the scratchpad tag
-#   riverctl spawn "$term -e nvim"
-#   sleep 0.5 # Wait for the application to launch
-#   riverctl set-focused-tags 11
-#   riverctl focus-output next
-# fi
+case "$1" in
+"term")
+  search="Special_term"
+  tag="$terminal_tag"
+  cmd="$term"
+  ;;
+"lf")
+  search="Special_lf"
+  tag="$lf_tag"
+  cmd="$lf"
+  ;;
+"btop")
+  search="Special_btop"
+  tag="$monitor_tag"
+  cmd="$btop"
+  ;;
+"nvim")
+  search="Special_nvim"
+  tag="$dots_tag"
+  cmd="$nvim"
+  ;;
+esac
+
+if lswt | grep -q "$search"; then
+  riverctl set-focused-tags "$tag"
+else
+  riverctl spawn "$cmd"
+  riverctl set-focused-tags "$tag"
+fi
