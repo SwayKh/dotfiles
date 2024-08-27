a13132e27efd0249c296527ecd24ba22044d0d6b
SwayKh <khatrisumit712@gmail.com>
2024-08-27 15:13:47 +0530

    (Nvim) Add performance opts to nvim-cmp, update doc scroll keybinds. Update gopls opts for LSP, Taken from Reddit

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -111,10 +111,15 @@ return {
         settings = {
           gopls = {
             completeUnimported = true,
-            usePlaceholders = true,
+            usePlaceholders = false,
             analyses = {
               unusedparams = true,
+              unreachable = true,
             },
+            -- report vulnerabilities
+            vulncheck = "Imports",
+            staticcheck = true,
+            gofumpt = true,
           },
         },
       },
