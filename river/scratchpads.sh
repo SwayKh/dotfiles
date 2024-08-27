2f1695cf8391653ee20b8702e80e2e7cd5f5703f
SwayKh <khatrisumit712@gmail.com>
2024-08-27 15:14:52 +0530

    (River) Update app-id names from special_* to special-*, in binds, scratchpads, rules and Add rules for steam and Brawlhalla

--- river/scratchpads.sh
+++ river/scratchpads.sh
@@ -16,29 +16,29 @@ lf_tag=$((1 << 12))
 dots_tag=$((1 << 13))
 monitor_tag=$((1 << 14))
 
-term="$terminal --app-id special_term -e zsh -ic \"tmux new-session -s Terminal\""
-lf="$terminal --app-id special_lf -e zsh -ic \"tmux new-session -s FileManager 'lf'\"" #lfMention
-btop="$terminal --app-id special_btop -e btop"
-nvim="$terminal --app-id special_nvim --working-directory=$HOME/dotfiles/ -e zsh -ic \"tmux new-session -s Editor 'nvim'\""
+term="$terminal --app-id special-term -e zsh -ic \"tmux new-session -s Terminal\""
+lf="$terminal --app-id special-lf -e zsh -ic \"tmux new-session -s FileManager 'lf'\"" #lfMention
+btop="$terminal --app-id special-btop -e btop"
+nvim="$terminal --app-id special-nvim --working-directory=$HOME/dotfiles/ -e zsh -ic \"tmux new-session -s Editor 'nvim'\""
 
 case "$1" in
 "term")
-  search="special_term"
+  search="special-term"
   tag="$terminal_tag"
   cmd="$term"
   ;;
 "lf")
-  search="special_lf"
+  search="special-lf"
   tag="$lf_tag"
   cmd="$lf"
   ;;
 "btop")
-  search="special_btop"
+  search="special-btop"
   tag="$monitor_tag"
   cmd="$btop"
   ;;
 "nvim")
-  search="special_nvim"
+  search="special-nvim"
   tag="$dots_tag"
   cmd="$nvim"
   ;;
