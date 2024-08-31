7a4f84d65da8941d04f5e5bdfb2f0c54759d583b
SwayKh <khatrisumit712@gmail.com>
2024-08-31 21:58:39 +0530

    (Nvim) Fix Treesitter autotag options, move the setup to nvim-ts-autotag plugin setup independently

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,50 +6,45 @@ return {
   event = { "VeryLazy", "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   dependencies = {
-    {
-      "windwp/nvim-ts-autotag",
-    },
+    "windwp/nvim-ts-autotag",
   },
   config = function()
     -- Start, Add support for dotfiles, taken from LazyVim
-    local ensure_installed = {
-      "c",
-      "cpp",
-      "go",
-      "lua",
-      "python",
-      "rust",
-      "bash",
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "tsx",
-      "json",
-      "vim",
-      "vimdoc",
-      "yaml",
-      "prisma",
-      "markdown",
-      "markdown_inline",
-      "dockerfile",
-      "gitignore",
-      "query",
-    }
-
     require("nvim-treesitter.configs").setup({
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = ensure_installed,
-
+      ensure_installed = {
+        "c",
+        "cpp",
+        "go",
+        "lua",
+        "python",
+        "rust",
+        "bash",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "dockerfile",
+        "gitignore",
+        "query",
+      },
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-      sync_install = false,
+      sync_install = true,
       auto_install = true,
       autopairs = { enable = true },
       rainbow = { enable = true },
       indent = { enable = true },
-      autotag = { enable = true },
       highlight = {
         enable = true,
+        use_languagetree = true,
         additional_vim_regex_highlighting = false,
       },
       incremental_selection = {
@@ -60,6 +55,24 @@ return {
           scope_incremental = "<c-s>",
           node_decremental = "<M-space>",
         },
+      },
+    })
+    require("nvim-ts-autotag").setup({
+      enable = true,
+      filetypes = {
+        "html",
+        "javascript",
+        "typescript",
+        "svelte",
+        "vue",
+        "tsx",
+        "jsx",
+        "rescript",
+        "css",
+        "lua",
+        "xml",
+        "php",
+        "markdown",
       },
     })
   end,
