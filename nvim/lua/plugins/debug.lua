7985573ce21b1540ceb674f50b37fb103247858b
SwayKh <khatrisumit712@gmail.com>
2024-09-01 23:20:18 +0530

    (Nvim) Remove all disabled plugins, haven't needed them in quite some time, can get the configs from git history
    But keep nvim-dap plugins, Might need them

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,6 +1,7 @@
 return {
   "mfussenegger/nvim-dap",
   event = "VeryLazy",
+  enabled = false,
   dependencies = {
     "rcarriga/nvim-dap-ui",
 
--- nvim/lua/plugins/disabled/undotree.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "mbbill/undotree",
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = "UndotreeToggle",
-  vim.keymap.set("n", "<leader>u", "<Cmd>UndotreeToggle<CR>", { silent = true, desc = "Open undo tree" }),
-}
