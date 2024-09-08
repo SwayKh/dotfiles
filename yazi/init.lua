227d97dbd6bc867239f26a686c0da05c516cbb3a
SwayKh <khatrisumit712@gmail.com>
2024-09-08 22:22:37 +0530

    (Yazi) Update yazi configuration to defaults provided from latest version update. Add One dark flavor.

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,19 +1,3 @@
-require("full-border"):setup()
-
-function Header:host()
-	if ya.target_family() ~= "unix" then
-		return ui.Line({})
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
-end
-
-function Header:render(area)
-	self.area = area
-
-	local right = ui.Line({ self:count(), self:tabs() })
-	local left = ui.Line({ self.host(), self:cwd(math.max(0, area.w - right:width())) })
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-	}
-end
+require("full-border"):setup({
+	type = ui.Border.ROUNDED,
+})
--- yazi/plugins/full-border.yazi/init.lua
+++ yazi/plugins/full-border.yazi/init.lua
@@ -1,31 +1,44 @@
-local function setup()
-	Manager.render = function(self, area)
-		local chunks = self:layout(area)
+local function setup(_, opts)
+	local type = opts and opts.type or ui.Border.ROUNDED
+	local old_build = Tab.build
 
+	Tab.build = function(self, ...)
 		local bar = function(c, x, y)
-			x, y = math.max(0, x), math.max(0, y)
-			return ui.Bar(ui.Rect { x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }, ui.Bar.TOP)
-				:symbol(c)
+			if x <= 0 or x == self._area.w - 1 then
+				return ui.Bar(ui.Rect.default, ui.Bar.TOP)
+			end
+
+			return ui.Bar(
+				ui.Rect({
+					x = x,
+					y = math.max(0, y),
+					w = ya.clamp(0, self._area.w - x, 1),
+					h = math.min(1, self._area.h),
+				}),
+				ui.Bar.TOP
+			):symbol(c)
 		end
 
-		return ya.flat {
-			-- Borders
-			ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
-			ui.Bar(chunks[1], ui.Bar.RIGHT),
-			ui.Bar(chunks[3], ui.Bar.LEFT),
-
-			bar("┬", chunks[1].right - 1, chunks[1].y),
-			bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
-			bar("┬", chunks[2].right, chunks[2].y),
-			bar("┴", chunks[2].right, chunks[1].bottom - 1),
-
-			-- Parent
-			Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
-			-- Current
-			Current:render(chunks[2]:padding(ui.Padding.y(1))),
-			-- Preview
-			Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
+		local c = self._chunks
+		self._chunks = {
+			c[1]:padding(ui.Padding.y(1)),
+			c[2]:padding(ui.Padding(c[1].w > 0 and 0 or 1, c[3].w > 0 and 0 or 1, 1, 1)),
+			c[3]:padding(ui.Padding.y(1)),
 		}
+
+		local style = THEME.manager.border_style
+		self._base = ya.list_merge(self._base or {}, {
+			ui.Border(self._area, ui.Border.ALL):type(type):style(style),
+			ui.Bar(self._chunks[1], ui.Bar.RIGHT):style(style),
+			ui.Bar(self._chunks[3], ui.Bar.LEFT):style(style),
+
+			bar("┬", c[1].right - 1, c[1].y),
+			bar("┴", c[1].right - 1, c[1].bottom - 1),
+			bar("┬", c[2].right, c[2].y),
+			bar("┴", c[2].right, c[2].bottom - 1),
+		})
+
+		old_build(self, ...)
 	end
 end
 
