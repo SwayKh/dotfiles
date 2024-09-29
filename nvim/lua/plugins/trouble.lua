b8758da7475d737015a8c40d9f3a3ca516cb2a01
SwayKh <khatrisumit712@gmail.com>
2024-09-29 17:49:29 +0530

    (Nvim) Add Trouble nvim plugin and autocmd to replace default quickfix list with trouble

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,41 @@
+return {
+  "folke/trouble.nvim",
+  event = "VeryLazy",
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false win.position=bottom<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=bottom<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+
+  config = function()
+    require("trouble").setup({})
+  end,
+}
