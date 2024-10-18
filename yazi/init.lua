a33d20179cb9d74497f58c8319c5751a7113e95e
SwayKh <khatrisumit712@gmail.com>
2024-10-18 17:07:10 +0530

    (Yazi) Move all Modified keymaps to prepend_keymap table and All removed keymaps to one commented block

--- yazi/init.lua
+++ yazi/init.lua
@@ -3,17 +3,16 @@ require("full-border"):setup({
 })
 
 require("personal"):setup()
-
 -- To Implement from lf
--- Pick Random function
--- Move to new directory
--- Drag n Drop function
--- Mappings
--- Settings
--- folder rules
--- Fix the statusline colors
--- Make all changed keymaps into prepend_keymap, keep the default config as is,  Easier to share
--- Fix the hovered file issue
--- Make the share command work with 0x0.st
--- Fix the Tabs no folder rules issue
+-- [Done] Pick Random function
+-- [Done] Move to new directory
+-- [Done] Drag n Drop function
+-- [Done] Mappings
+-- [Done] Settings
+-- [Done] folder rules
+-- [Done] Fix the statusline colors
+-- [Done] Make all changed keymaps into prepend_keymap, keep the default config as is,  Easier to share
+-- [Done] Fix the hovered file issue
+-- [Done] Make the share command work with 0x0.st
+-- [Done] Fix the Tabs no folder rules issue
 -- Archive functionality
