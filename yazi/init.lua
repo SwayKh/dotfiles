a21cbf07b8dfbf46da474389e25f3f3fb7310e5b
SwayKh <khatrisumit712@gmail.com>
2025-06-02 00:21:59 +0530

    (Yazi) Remove task list comments from yazi config. Disable hidden files by default.

--- yazi/init.lua
+++ yazi/init.lua
@@ -3,16 +3,3 @@ require("full-border"):setup({
 })
 
 require("personal"):setup()
--- To Implement from lf
--- [Done] Pick Random function
--- [Done] Move to new directory
--- [Done] Drag n Drop function
--- [Done] Mappings
--- [Done] Settings
--- [Done] folder rules
--- [Done] Fix the statusline colors
--- [Done] Make all changed keymaps into prepend_keymap, keep the default config as is,  Easier to share
--- [Done] Fix the hovered file issue
--- [Done] Make the share command work with 0x0.st
--- [Done] Fix the Tabs no folder rules issue
--- Archive functionality
