9d8815d9f0a05509ee186cd3f9745250863dca7a
SwayKh <khatrisumit712@gmail.com>
2025-06-04 23:35:07 +0530

    (River) Small variable fix in scratchpad script in river

--- river/scratchpads.sh
+++ river/scratchpads.sh
@@ -22,7 +22,7 @@ btop="$terminal --app-id special-btop -e zsh -ic btop"
 nvim="$terminal --app-id special-nvim --working-directory=$HOME/dotfiles/ -e zsh -ic \"tmux new-session -s Editor 'nvim && zsh'\""
 files="$terminal --app-id special-files -e zsh -ic yazi"
 
-projectTerm="footclient --app-id special-project -e zsh -ic '$HOME/scripts/project.tmux'"
+projectTerm="$terminal --app-id special-project -e zsh -ic '$HOME/scripts/project.tmux'"
 
 case "$1" in
 "term")
