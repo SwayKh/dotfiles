de6669491bf8182fb1288f1bd8298ca63c3007d4
SwayKh <khatrisumit712@gmail.com>
2025-06-05 22:24:58 +0530

    (Dunst) Update launch-dunst.sh script to match dunst colors with waybar

--- dunst/launch-dunst.sh
+++ dunst/launch-dunst.sh
@@ -5,8 +5,8 @@ source "$HOME/.cache/wal/colors.sh"
 conf="$HOME/.config/dunst/dunstrc"
 
 # update dunst colors on Pywal16 colors.
-sed -i '/background = /s|.*|    background = "'$color8'"|' "$conf"
-sed -i '/foreground = /s|.*|    foreground = "'$color6'"|' "$conf"
-sed -i '/frame_color = /s|.*|    frame_color = "'$color6'"|' "$conf"
+sed -i '/background = /s|.*|    background = "'$color2'"|' "$conf"
+sed -i '/foreground = /s|.*|    foreground = "'$color0'"|' "$conf"
+sed -i '/frame_color = /s|.*|    frame_color = "'$color0'"|' "$conf"
 
 pkill dunst && notify-send "Wallpaper and colorscheme changed; setting RGB value"
