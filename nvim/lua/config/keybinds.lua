4f9257d640e02bc42c8ab26670dfc0834d6ea3db
SwayKh <khatrisumit712@gmail.com>
2025-07-08 01:11:40 +0530

    (Nvim) Remove unneeded keybinds and also remove the reloadConfig util.

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -2,7 +2,6 @@
 
 local map = vim.keymap.set
 
--- Misc
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
@@ -14,12 +13,6 @@ map("n", "<leader>X", "<Cmd>source %<CR>", { silent = true, desc = "Source curre
 map("n", "<space>x", "<Cmd>.lua<CR>", { desc = "Execute selected lua lines" })
 map("v", "<space>x", ":lua<CR>", { desc = "Execute selected lua lines" })
 
--- Reload all lua files under neovim config
--- map("n", "<leader>.", "<Cmd>source ~/.config/nvim/init.lua<CR>", { silent = true, desc = "Source nvim config" })
-map("n", "<leader>rc", function()
-  require("config.utils").reloadConfig()
-end, { silent = true, desc = "Source nvim config" })
-
 -- Go to Beginning or End of line
 map({ "n", "v" }, "H", "_^", { desc = "Move to the beginning of the line" })
 map({ "n", "v" }, "L", "$", { desc = "Move to the end of the line" })
@@ -30,12 +23,6 @@ map("n", "<C-k>", "<C-w>k", { desc = "Move Between splits with HJKL" })
 map("n", "<C-h>", "<C-w>h", { desc = "Move Between splits with HJKL" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move Between splits with HJKL" })
 
--- Move in insert mode, And Command-line mode
-map({ "i", "c" }, "<C-j>", "<Down>", { desc = "Move Down in insert mode" })
-map({ "i", "c" }, "<C-k>", "<Up>", { desc = "Move Up in insert mode" })
-map({ "i", "c" }, "<C-h>", "<Left>", { desc = "Move Left in insert mode" })
-map({ "i", "c" }, "<C-l>", "<Right>", { desc = "Move Right in insert mode" })
-
 -- Move selected text up/down
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
@@ -43,10 +30,6 @@ map("v", "K", ":m '<-2<CR>gv=gv")
 -- Move through long lines of text visually
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Scroll keybinds
-map({ "n", "v" }, "<M-j>", "5j", { desc = "Scroll 5 lines down" })
-map({ "n", "v" }, "<M-k>", "5k", { desc = "Scroll 5 lines up" })
 
 -- Split keybinds, Need to figure out a better system
 map("n", "<leader>V", "<Cmd>vsplit<CR>", { desc = "Split [V]ertically" })
@@ -65,11 +48,6 @@ map("n", "qq", "<Cmd>%bd<CR><Cmd>lua Snacks.dashboard()<CR>", { silent = true, d
 map({ "n", "i", "v" }, "<C-s>", "<ESC><Cmd>w!<CR>", { silent = true, desc = "[S]ave all files" })
 map({ "n", "i", "v" }, "<C-q>", "<ESC><Cmd>wqa!<CR>", { silent = true, desc = "Save all file and [Q]uit" })
 
--- Buffer keybinds
--- map("n", "<leader>bd", "<Cmd>bdelete<CR>", { silent = true, desc = "Run [B]buffer [D]elete" })
-map("n", "<leader>bn", "<Cmd>bnext<CR>", { silent = true, desc = "Run [B]buffer [N]ext" })
-map("n", "<leader>bp", "<Cmd>bprevious<CR>", { silent = true, desc = "Run [B]buffer [P]revious" })
-
 -- Open Lazy/Mason UIs and reload plugins
 -- map("n", "<leader>ol", "<Cmd>Lazy<CR>", { silent = true, desc = "Open [L]azy" })
 map("n", "<leader>om", "<Cmd>Mason<CR>", { silent = true, desc = "Open [M]ason" })
@@ -79,13 +57,3 @@ map("n", "<leader>cn", "<Cmd>cnext<CR>", { desc = "Go to next item in Quickfix l
 map("n", "<leader>cp", "<Cmd>cprev<CR>", { desc = "Go to previous item in Quickfix list" })
 map("n", "<leader>co", "<Cmd>copen<CR>", { desc = "Open Quickfix List" })
 map("n", "<leader>cc", "<Cmd>cclose<CR>", { desc = "Close Quickfix List" })
-
--- Remap common typos when saving and closing files
-vim.cmd("cnoreabbrev Q  q")
-vim.cmd("cnoreabbrev q1  q!")
-vim.cmd("cnoreabbrev Q1  q!")
-vim.cmd("cnoreabbrev Qa1 qa!")
-vim.cmd("cnoreabbrev Qa qa")
-vim.cmd("cnoreabbrev W  w")
-vim.cmd("cnoreabbrev Wq wq")
-vim.cmd("cnoreabbrev WQ wq")
