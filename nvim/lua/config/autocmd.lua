6276be584a7e0260ae50e5f6631c3bc5005a80a1
SwayKh <khatrisumit712@gmail.com>
2025-07-08 01:17:09 +0530

    (Nvim) Create CmpHighlight.lua to add background color to kind_icons color in Blink.cmp, Use color from Mini.Icons
    Also removed background from Pmenu in Colorscheme autocmd.

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -44,6 +44,7 @@ autocmd("ColorScheme", {
   pattern = "*",
   callback = function()
     vim.opt.background = "dark"
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
