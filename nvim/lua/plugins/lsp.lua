8266eb5aa3c11918a4a24d592d146cfa35ee8d9a
SwayKh <khatrisumit712@gmail.com>
2025-07-12 13:51:35 +0530

    (Nvim) Remove cursorhold highlighting autcmd from lsp configuration. And remove icons from vim.diagnostic.config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -53,45 +53,12 @@ vim.diagnostic.config({
     source = "if_many",
     spacing = 2,
     prefix = "",
-    format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = "󰅚 " .. diagnostic.message,
-        [vim.diagnostic.severity.WARN] = "󰀪 " .. diagnostic.message,
-        [vim.diagnostic.severity.INFO] = "󰋽 " .. diagnostic.message,
-        [vim.diagnostic.severity.HINT] = "󰌶 " .. diagnostic.message,
-      }
-      return diagnostic_message[diagnostic.severity]
-    end,
   },
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
   callback = function(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.server_capabilities.documentHighlightProvider then
-      local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.document_highlight,
-      })
-
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-
-    vim.api.nvim_create_autocmd("LspDetach", {
-      group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-      callback = function(event2)
-        vim.lsp.buf.clear_references()
-        vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-      end,
-    })
-
     vim.api.nvim_buf_create_user_command(event.buf, "Format", function(_)
       vim.lsp.buf.format()
     end, { desc = "Format current buffer with LSP" })
