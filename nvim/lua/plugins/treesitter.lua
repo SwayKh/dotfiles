eb3e8617e228832cc67267f9941b37b24428a8a9
SwayKh <khatrisumit712@gmail.com>
2025-07-12 13:14:30 +0530

    (Nvim) Remove ensure_installed language from tree-sitter, and remove disable config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,4 @@
--- Start, Add support for dotfiles, taken from LazyVim
 require("nvim-treesitter.configs").setup({
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = {
-    "c",
-    "lua",
-    "vim",
-    "vimdoc",
-    "markdown",
-    "markdown_inline",
-    "gitignore",
-    "query",
-    "regex",
-    -- "cpp",
-    -- "go",
-    -- "python",
-    -- "rust",
-    -- "bash",
-    -- "html",
-    -- "css",
-    -- "javascript",
-    -- "typescript",
-    -- "tsx",
-    -- "json",
-    -- "yaml",
-    -- "prisma",
-    -- "dockerfile",
-    -- "rasi",
-    -- "hyprlang",
-  },
-  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   sync_install = true,
   auto_install = true,
   autopairs = { enable = true },
@@ -38,15 +8,6 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     use_languagetree = true,
     additional_vim_regex_highlighting = false,
-  },
-  incremental_selection = {
-    enable = false,
-    keymaps = {
-      init_selection = "<c-space>",
-      node_incremental = "<c-space>",
-      scope_incremental = "<c-s>",
-      node_decremental = "<M-space>",
-    },
   },
 })
 require("nvim-ts-autotag").setup({
