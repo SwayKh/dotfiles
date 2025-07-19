86369cf87e317d823a3f38dc8edfcd3358d86642
SwayKh <khatrisumit712@gmail.com>
2025-07-19 18:18:44 +0530

    (Pywal) Update pywal scripts to run swaync-client -rs and not dunst launch script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -26,7 +26,7 @@ alias lf=lfcd
 alias yazi=y
 alias lg=lazygit
 alias hw='hellwal --no-cache -i $HOME/.config/wall.png --bright-offset 0.3'
-alias pw='wal -n -e -a 80 -i $HOME/.config/wall.png -o $HOME/.config/dunst/launch-dunst.sh'
+alias pw='wal -n -e -a 80 -i $HOME/.config/wall.png && swaync-client -rs'
 
 # cd Aliases
 alias ..='cd ..'
