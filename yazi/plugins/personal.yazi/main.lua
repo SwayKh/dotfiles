3b4200d37ae9647e006b99cdd0071087b23011c3
SwayKh <khatrisumit712@gmail.com>
2026-02-03 22:47:15 +0530

    (Yazi) Add openWithCmd plugin code to personal.yazi

--- yazi/plugins/personal.yazi/main.lua
+++ yazi/plugins/personal.yazi/main.lua
@@ -158,6 +158,24 @@ local function chmodx()
 	end
 end
 
+local function openWithCmd(job)
+	local block = job.args[1] and job.args[1] == "block"
+
+	local value, event = ya.input({
+		title = block and "Open with (block):" or "Open with:",
+		pos = { "hovered", y = 1, w = 50 },
+	})
+
+	if event == 1 then
+		local s = ya.target_family() == "windows" and " %*" or ' "$@"'
+		ya.emit("shell", {
+			value .. s,
+			block = block,
+			orphan = not block,
+		})
+	end
+end
+
 local function chmod()
 	ya.emit("escape", { visual = true })
 
@@ -266,6 +284,8 @@ local function entry(_, job)
 		chmodx()
 	elseif job.args[1] == "share" then
 		share()
+	elseif job.args[1] == "openWithCmd" then
+		openWithCmd(job)
 	end
 end
 
